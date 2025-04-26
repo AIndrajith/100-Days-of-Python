975d62921d2a27a5a00d971e5706f7582d353829
AIndrajith <deegalla12378@usci.ruh.ac.lk>
2025-04-26 23:33:45 +0530

    day 40 mini projects

---
+++ Day 40/Mini Weather API/app.py
@@ -0,0 +1,50 @@
+# MIni Weather API
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# Modk Weather Data
+weather_data = {
+    "new york":{"temperature":22, "condition":"sunny"},
+    "london": {"temperature":15, "condition":"Cloudy"},
+    "tokyo": {"temperature":28, "condition":"Clear"},
+    "sydney":{"temperature": 18, "condition":"Rainy"}
+}
+
+# Roo Endpoint
+@app.route('/')
+def home():
+    return jsonify({"message":"Welcome to the Mini Weather API!"})
+
+# Get Weather for all cities
+@app.route('/weather', methods=['GET'])
+def get_all_weather():
+    return jsonify(weather_data)
+
+# Get Weather for a specific City
+@app.route('/weather/<city>', methods=['GET'])
+def get_weather_by_city(city):
+    city = city.lower()
+    if city in weather_data:
+        return jsonify({city: weather_data[city]})
+    return jsonify({"error":"city not found"}), 404
+
+# Add new weather data
+@app.route('/weather',methods=['POST'])
+def add_city_weather():
+    data = request.json
+    city = data.get('city','').lower()
+    temperature = data.get('temperature')
+    condition = data.get('condition')
+
+    if not city or not temperature or not condition:
+        return jsonify({'error': 'Missing city, temperature or condition'}),400 
+    
+    weather_data[city] = {"temperature": temperature, "condition": condition}
+    return jsonify({"message":f"weather for {city} added successfully"}), 201
+
+
+# Run App
+if __name__=='__main__':
+    app.run(debug=True)
